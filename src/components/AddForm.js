69d5db18bd7e31edd4031b1151f661663f92dc98
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-10-18 00:35:39 +0300

    refacto: AddForm component revised.

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,14 +1,43 @@
 import { Form, Button } from 'react-bootstrap';
+import { useContext, useState } from 'react';
+
+// Context
+import { EmployeeContext } from '../contexts/EmployeeContext';
 
 const AddForm = () => {
 
+    const { addEmployee } = useContext(EmployeeContext);
+
+    // const [name, setName] = useState("");
+    // const [email, setEmail] = useState("");
+    // const [address, setAddress] = useState("");
+    // const [phone, setPhone] = useState("");
+
+    const [newEmployee, setNewEmployee] = useState({
+        name:"", email:"", address: "", phone: ""
+    })
+
+    const {name, email, address, phone } = newEmployee;
+    
+    const onInputChange = (e) => {
+        setNewEmployee({...newEmployee, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addEmployee(name, email, address, phone)
+    }
+
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
 
             <Form.Group>
                 <Form.Control
                     type="text"
                     placeholder="Name *"
+                    name="name"
+                    value={name}
+                    onChange={e => onInputChange(e)}
                     required 
                 />
             </Form.Group>
@@ -17,6 +46,9 @@ const AddForm = () => {
                 <Form.Control
                     type="email"
                     placeholder="Email *"
+                    name="email"
+                    value={email}
+                    onChange={e => onInputChange(e)}
                     required 
                 />
             </Form.Group>
@@ -25,6 +57,9 @@ const AddForm = () => {
                 <Form.Control
                     as="textarea"
                     placeholder="Address *"
+                    name="address"
+                    value={address}
+                    onChange={e => onInputChange(e)}
                     rows={3} 
                 />
             </Form.Group>
@@ -33,6 +68,9 @@ const AddForm = () => {
                 <Form.Control
                     type="text"
                     placeholder="Phone"
+                    name="phone"
+                    value={phone}
+                    onChange={e => onInputChange(e)}
                 />
             </Form.Group>
 
