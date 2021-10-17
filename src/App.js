3dceca18d3618eb2e32d1a94d051ea219d257b19
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-10-17 23:20:47 +0300

    refactor: App.js revised.

--- src/App.js
+++ src/App.js
@@ -8,26 +8,7 @@ function App() {
     <div classNameName="App">
       <div className="container-xl">
         <div className="table-responsive">
-          <div className="table-wrapper">
-            <div className="table-title">
-              <div className="row">
-                <div className="col-sm-6">
-                  <h2>
-                    Manage <b>Employees</b>
-                  </h2>
-                </div>
-                <div className="col-sm-6">
-                  <a
-                    href="#addEmployeeModal"
-                    className="btn btn-success"
-                    data-toggle="modal"
-                  >
-                    <i className="material-icons">&#xE147;</i>{" "}
-                    <span>Add New Employee</span>
-                  </a>
-                </div>
-              </div>
-            </div>
+          <div className="table-wrapper">            
             <EmployeeContextProvider>
               <EmployeeList />
             </EmployeeContextProvider>
