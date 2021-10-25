686755de54dde3eb6cd23662ae2ed247ae815db0
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-10-25 19:22:46 +0300

    refactor: App.js revised.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+// Routing
+import { BrowserRouter,  Route , Switch} from "react-router-dom";
+
+// Components
 import EmployeeList from "./components/EmployeeList";
+import LoginForm from "./components/LoginForm";
 
 // Context
 import EmployeeContextProvider from "./contexts/EmployeeContext";
 
+
 function App() {
   return (
+    <BrowserRouter>
     <div classNameName="App">
       <div className="container-xl">
         <div className="table-responsive">
           <div className="table-wrapper">            
             <EmployeeContextProvider>
-              <EmployeeList />
+              <Route exact path = "/" component={LoginForm}  />
+              <Route exact path = "/home" component={EmployeeList}  />
             </EmployeeContextProvider>
           </div>
         </div>
-      </div>
+      </div> 
     </div>
+    </BrowserRouter>
   );
 }
 
